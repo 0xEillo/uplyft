3d75531d1de2f3966f68e2f6694eff247e486335
ollie <67156692+0xEillo@users.noreply.github.com>
2025-10-22 22:11:53 +0100

    tryfix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,17 +1,30 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import type { AsyncStorageStatic } from '@react-native-async-storage/async-storage'
+import type { SupabaseClientOptions } from '@supabase/supabase-js'
 import { createClient, processLock } from '@supabase/supabase-js'
 import 'react-native-url-polyfill/auto'
+
+const isReactNative =
+  typeof navigator !== 'undefined' && navigator.product === 'ReactNative'
+const isServer = typeof window === 'undefined'
+
+let asyncStorage: AsyncStorageStatic | undefined
+
+if (isReactNative) {
+  asyncStorage = require('@react-native-async-storage/async-storage').default
+}
+
+const supabaseOptions: SupabaseClientOptions<'public'> = {
+  auth: {
+    storage: asyncStorage,
+    autoRefreshToken: !isServer,
+    persistSession: !isServer,
+    detectSessionInUrl: false,
+    lock: processLock,
+  },
+}
 
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
   process.env.EXPO_PUBLIC_SUPABASE_KEY!,
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-      lock: processLock,
-    },
-  },
+  supabaseOptions,
 )
