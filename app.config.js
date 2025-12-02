ca3d5bf9889d7a0e0be80aef81cc99a0e401207a
ollie <67156692+0xEillo@users.noreply.github.com>
2025-12-02 15:57:12 +0000

    version bump

--- app.config.js
+++ app.config.js
@@ -52,7 +52,7 @@ module.exports = {
   expo: {
     name: 'Rep AI',
     slug: 'repai',
-    version: '1.0.5',
+    version: '1.0.6',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'repai',
